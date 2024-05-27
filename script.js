876d2bd928064162967b326a66ac03cc1ddf1ea0
Faridah-11 <abdulwahabfaridah11@gmail.com>
2024-05-27 03:51:13 +0100

    Corrected some lines

--- script.js
+++ script.js
@@ -6,18 +6,16 @@ const pop_ups = document.querySelectorAll('.pop_up');
 const submitBtn = document.querySelector('button');
 const inputs = document.querySelectorAll('input');
 
-let condition = function(){
-    pop_ups.forEach(pop_up => {
-    (pop_up.classList.contains('hidden'))
-})
-}
+let condition = function() {
+    return Array.from(pop_ups).every(pop_up => pop_up.classList.contains('hidden'));
+};
 
 let checkForm = function(){
     inputs.forEach(input => {
         if (!(input.value || input.checked || message.value)){
             document.querySelector(`.${input.name}_pop_up`).classList.remove('hidden');
             document.querySelector(`#${input.id}`).classList.add('error');
-            return
+        
     }})
     if(!(message.value)){
         document.querySelector(`#${message.id}`).classList.add('error');
@@ -27,24 +25,27 @@ let checkForm = function(){
         document.querySelector('.query_pop_up').classList.remove('hidden')
         return
     }
-}
+};   
 // checkForm()
 const validateForm = function(){
     checkForm(); 
-    condition();
-    if(!condition() == true){
-        // console.log('done')
+   let isConditionMet= condition(); 
+    if(isConditionMet){
+        console.log('done')
         document.querySelector('.success').classList.remove('hidden')
+        message.value = '';
         inputs.forEach(input => {
             input.value = ''
             input.checked = ''
         })
-    } return        
+    } else{
+        console.log('not done')
+    }       
 }
 
 submitBtn.addEventListener('click', function(e){
     e.preventDefault();
-    validateForm()
+   validateForm()
 })
 
 
